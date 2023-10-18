8b255f0a9e75921fd63a0e7cc63a1f5cc0185a48
futu2 <112805308+futu2@users.noreply.github.com>
2023-10-18 05:37:36 -0600

    Temp plain callCabal2nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "h-raylib";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    let
+      perSystem = system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; };
+
+          haskellPackages = pkgs.haskellPackages.override {
+            # overrides = self: super: {
+            #   h-raylib =
+            #     pkgs.haskell.lib.doJailbreak
+            #     (pkgs.haskell.lib.unmarkBroken super.h-raylib);
+            # };
+          };
+
+          jailbreakUnbreak = pkg:
+            pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
+
+          packageName = "h-raylib";
+        in
+        {
+          packages.${packageName} = # (ref:haskell-package-def)
+            haskellPackages.callCabal2nix packageName ./. rec {
+              # Dependency overrides go here
+              c=null;
+            };
+
+          defaultPackage = inputs.self.packages.${system}.${packageName};
+          devShell = inputs.self.packages.${system}.${packageName}.env.overrideAttrs (oldEnv: { buildInputs = oldEnv.buildInputs ++ [ haskellPackages.haskell-language-server ]; });
+        };
+    in
+    inputs.flake-utils.lib.eachDefaultSystem perSystem;
+}
